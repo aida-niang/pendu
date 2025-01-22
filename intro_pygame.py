6b04c21f301ea4913413bb8cc9e41816e1e706d0
amina-taleb <amina.taleb@laplateforme.io>
2025-01-22 14:35:49 +0100

    show images one by one using pygame

--- intro_pygame.py
+++ intro_pygame.py
@@ -15,16 +15,34 @@ green = (0, 255, 0)
 blue = (50, 153, 213)
 
 #Create the screen
-width = 800
+width = 600
 height = 600
 screen = pygame.display.set_mode((width,height))
 title = pygame.display.set_caption('Hangman Game')
 
+#I want to print objects on my screen :
+#######Method1 : show images
+# step1 = pygame.image.load('pendu_image\image1.png')
+# step2 = pygame.image.load('pendu_image\image2.png')
+# step3 = pygame.image.load('pendu_image\image3.png')
+# step4 = pygame.image.load('pendu_image\image4.png')
+# step5 = pygame.image.load('pendu_image\image5.png')
+# step6 = pygame.image.load('pendu_image\image6.png')
+# step7 = pygame.image.load('pendu_image\image7.png')
 
-#Create the objects :
-object_1 = pygame.rect((10, 10))
+#I want to use a loop to import all the images using one variable:
+#Initialize an empty list
+images = []
+for i in range(1, 8):
+    image = pygame.image.load(f'pendu_image\image{i}.png')  
+    images.append(image)
+
+
+######Method2 : Create the objects :
+#object_1 = pygame.draw.line(()) #to perform later ....
 
 #The main loop of the game :
+image_index = 0  
 running = True
 while running :
 
@@ -33,11 +51,19 @@ while running :
             running = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos() #how you get the position of the mouse. gives the position of the mouse, especially useful when clicking
-            print(pos)
+            pos = pygame.mouse.get_pos() #this function allows to print the positions of the objects, just by clicking on the screen
+            print(pos) #print the positions in the terminal
 
-    screen.fill((green))
-    screen.blit(object_1, (0, 0)) #this 
+        if event.type == pygame.KEYDOWN: # When we press a key , that changes the hangman image 
+            if event.key == pygame.K_RETURN :
+                image_index += 1 #when all the images are shown, we exit
+
+                
+
+    screen.fill(white) #choose the color of the screen
+
+    screen.blit(images[image_index], (150, 150)) #show the images and use a keyboard to change the image
+
     pygame.display.flip()
 
 pygame.quit()
